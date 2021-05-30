a2cc2a8519daad4c9d14da3644e38dd2d46a3ff4
Stefan Chalupka <50046445+chalupka95@users.noreply.github.com>
2021-05-30 21:51:34 +0200

    Update FPGrowth.py

--- FPGrowth.py
+++ FPGrowth.py
@@ -3,16 +3,42 @@ from fpgrowth_py.utils import *
 import pprint as pprint
 pp = pprint.PrettyPrinter(indent=4)
 
-itemSetList = [['MONKEY'],
-                ['DONKEY'],
-                ['MAKE'],
-                ['MUCKY'],
-                ['COOKIE']]
+itemSetList=[]
+#itemSetList = [['MONKEY'],['DONKEY'],['MAKE'],['MUCKY'],['COOKIE']]
+
+def sortLength(item):
+    return len(item)
+
+
+####Read Dataset for Algo####
+def read_accident():
+    file_path='C:/Users/Stefan Chalupka/Desktop/DataScience/sample.txt'
+    datei=open(file_path, 'r')
+
+    for i in datei:
+        line=datei.readline()
+        liste=line.split(' ')
+        if liste[-1]=='\n':
+            liste.pop()
+        itemSetList.append(liste)
+
+
 
 #freqItemSet, rules = fpgrowth(itemSetList, minSup=0.5, minConf=0.5)
-freqItemSet, rules = fpgrowth(itemSetList, 0.51, 0.5)
 #pp.pprint(rules)
-pp.pprint(freqItemSet)
+
+
+####Start Algo####
+read_accident()
+freqItemSet, rules = fpgrowth(itemSetList, 0.7, 0.7)
+
+
+####Print Result####
+freqItemSet.sort(reverse = True, key = sortLength)
+for i in freqItemSet:
+    if len(i) > 4:
+        print(sorted(i))
+#pp.pprint(freqItemSet)
 
 
 
