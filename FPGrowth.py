270bb2c7324759e4f711c24ac3acbe16143e3865
Stefan Chalupka <50046445+chalupka95@users.noreply.github.com>
2021-05-30 22:19:59 +0200

    Update FPGrowth.py

--- FPGrowth.py
+++ FPGrowth.py
@@ -6,15 +6,29 @@ pp = pprint.PrettyPrinter(indent=4)
 itemSetList=[]
 #itemSetList = [['MONKEY'],['DONKEY'],['MAKE'],['MUCKY'],['COOKIE']]
 
+
+#Hilfsfunktionen
 def sortLength(item):
     return len(item)
 
 
-####Read Dataset for Algo####
+
+    
+####Read Dataset Accident for Algo####
 def read_accident():
     file_path='C:/Users/Stefan Chalupka/Desktop/DataScience/sample.txt'
     datei=open(file_path, 'r')
-
+    for i in datei:
+        line=datei.readline()
+        liste=line.split(' ')
+        if liste[-1]=='\n':
+            liste.pop()
+        itemSetList.append(liste)
+    
+####Read Dataset Retail for Algo####
+def read_retail():
+    file_path='C:/Users/Stefan Chalupka/Desktop/DataScience/retail.dat'
+    datei=open(file_path, 'r')
     for i in datei:
         line=datei.readline()
         liste=line.split(' ')
@@ -22,23 +36,62 @@ def read_accident():
             liste.pop()
         itemSetList.append(liste)
 
-
-
-#freqItemSet, rules = fpgrowth(itemSetList, minSup=0.5, minConf=0.5)
-#pp.pprint(rules)
-
-
-####Start Algo####
-read_accident()
-freqItemSet, rules = fpgrowth(itemSetList, 0.7, 0.7)
-
-
-####Print Result####
-freqItemSet.sort(reverse = True, key = sortLength)
-for i in freqItemSet:
-    if len(i) > 4:
-        print(sorted(i))
-#pp.pprint(freqItemSet)
+####Read Dataset Entree for Algo####
+#def read_entree():
+#    file_path='C:/Users/Stefan Chalupka/Desktop/DataScience/retail.dat'
+#    datei=open(file_path, 'r')
+#    for i in datei:
+#        line=datei.readline()
+#        liste=line.split(' ')
+#        if liste[-1]=='\n':
+#            liste.pop()
+#        itemSetList.append(liste)
 
 
 
+####Print result of Algorithm####
+def print_result(dataset, freqItemSet, rules):
+    if dataset == 'accident':
+        freqItemSet.sort(reverse = True, key = sortLength)
+        for i in freqItemSet:
+            if len(i) > 4:
+                print(sorted(i))
+        #pp.pprint(freqItemSet)
+
+        #freqItemSet, rules = fpgrowth(itemSetList, minSup=0.5, minConf=0.5)
+        #pp.pprint(rules)
+
+    elif dataset == 'retail':
+        pp.pprint(freqItemSet)
+
+        #freqItemSet, rules = fpgrowth(itemSetList, minSup=0.5, minConf=0.5)
+        #pp.pprint(rules)
+        
+    elif dataset == 'entree':
+        pp.pprint(freqItemSet)
+
+        #freqItemSet, rules = fpgrowth(itemSetList, minSup=0.5, minConf=0.5)
+        #pp.pprint(rules)
+
+    else:
+        print("Please Use correct Dataset:\nMain('accident')\nMain('retail')\nMain('entree')")
+
+#Start Program with Dataset
+def Main(dataset, minSup=0.5, minConf=0.5):
+    if dataset == 'accident':
+        read_accident()
+    elif dataset == 'retail':
+        read_retail()
+    elif dataset == 'entree':
+        read_entree()
+    else:
+        print("Please Use correct Dataset:\nMain('accident')\nMain('retail')\nMain('entree')")
+
+
+
+    ####Start Algo####
+    freqItemSet, rules = fpgrowth(itemSetList, minSup, minConf)
+    print_result(dataset, freqItemSet, rules )
+    
+#Main('accident', 0.7, 0.7)
+Main('retail', 0.3, 0.3)
