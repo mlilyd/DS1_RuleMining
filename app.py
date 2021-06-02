20b0db7c3ace3b768da5af1dd2ffc70165eb0140
Lily D <lily@stud.uni-heidelberg.de>
2021-06-02 23:58:15 +0200

    uploaded entree dataset, as well as streamlit app.py and ECLAT.py

---
+++ app.py
@@ -0,0 +1,70 @@
+import streamlit as st
+import numpy as np
+import pandas as pd
+import time
+
+
+#import dataset reading functions from other modules
+from entree import read_entree_dataset
+
+"""
+# Data Science 1 Project: Rule Mining
+by Seida Basha, Stefan Chalupka, Lily Djami
+"""
+
+
+ds_selection = st.selectbox(
+    'Select a dataset',
+    ["Entree Dataset", "Accidents", "Seida's"])
+
+#dummy dataset for initialization
+dataset = pd.DataFrame()
+
+if ds_selection == "Entree Dataset":
+  dataset = read_entree_dataset()
+elif ds_selection == "Accidents":
+  dataset = pd.DataFrame()
+elif ds_selection == "Seida's":
+  dataset = pd.DataFrame() 
+
+dataset
+
+"""
+Here are some examples for Streamlit
+"""
+algorithm = st.selectbox(
+    'Choose an algorithm',
+    ["A-Priori", "ECLAT", "FP-Growth"])
+
+
+'You selected:', algorithm
+
+
+df = pd.DataFrame({
+  'first column': [1, 2, 3, 4],
+  'second column': [10, 20, 30, 40]
+})
+
+chart_data = pd.DataFrame(
+     np.random.randn(20, 3),
+     columns=['a', 'b', 'c'])
+
+st.line_chart(chart_data)
+
+'Starting a long computation...'
+
+# Add a placeholder
+latest_iteration = st.empty()
+bar = st.progress(0)
+
+for i in range(100):
+  # Update the progress bar with each iteration.
+  latest_iteration.text(f'Iteration {i+1}')
+  bar.progress(i + 1)
+  time.sleep(0.1)
+
+'...and now we\'re done!'
+
+    
+
+
