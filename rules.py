261922fd23ed53ee03e77de1afe8e0db06c474b9
Lily D <lily@stud.uni-heidelberg.de>
2021-06-20 15:30:28 +0200

    Added rule to str

--- rules.py
+++ rules.py
@@ -25,3 +25,15 @@ def rules_from_support(support, min_support=0, separator=" & "):
     df['itemsets'] = [frozenset(i.split(separator)) for i in df['itemsets']]
     rules = association_rules(df, min_threshold=min_support)
     return rules
+
+
+def frozenset_to_str(t):
+    t = list(t)
+    t.sort()
+    return "(" + ", ".join([str(i) for i in t]) + ")"
+
+def rule_str(rule_df):
+    rule_list = [frozenset_to_str(rule_df['antecedents'][i]) + " -> " + frozenset_to_str(rule_df['consequents'][i])
+                for i in range(rule_df.shape[0])]
+
+    rule_df['rule'] = rule_list
